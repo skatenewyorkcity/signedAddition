bde2197185d324835927f825d682196a42f27128
Pedro Garboza <p.garboza@gmail.com>
2017-04-13 22:29:55 -0400

    Added Project1

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int decToBinary(int n);
+
+int main(){
+	int numVal;
+
+	cout << "Input a number (between 0 to 255): ";
+	cin >> numVal;
+
+	cout << decToBinary(numVal) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int decToBinary(int n){
+
+	return 0;
+}
